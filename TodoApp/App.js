f07abf46a15e5b648486d8bb8d38af01a231def5
Ilan Y <ilanydg@gmail.com>
2021-05-22 21:01:50 +0300

    Added input

--- TodoApp/App.js
+++ TodoApp/App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>ToDo List</Text>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput}></TextInput>
+        <Button title="Add" />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  title:{
+    fontSize: 64,
+    color:'lightgrey'
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    // justifyContent: "center",
   },
+  inputContainer: {
+    flexDirection: "row",
+    width:'70%',
+    justifyContent:"space-between"
+  },
+  textInput:{
+    borderColor: 'grey',
+    borderWidth: 2,
+    width: '70%'
+  }
 });
